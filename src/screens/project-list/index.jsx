6bee6844dd2b8e7764652329a7e2c935e0bb8614
左国梁 <3059349417@qq.com>
2021-06-09 23:40:35 +0800

    更新

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,9 +1,20 @@
 import {SearchPanel} from './search-panel'
 import {List} from './list'
+import React, { useState, useEffect } from 'react'
 
 export const ProjectListScreen = (params) => {
+    const [param, setParam] = useState({name:'',personId:''})
+    const [list, setList] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:3001/projects').then(async response=> {
+            if(response.ok) {
+                setList(await response.json())
+            }
+        })
+    },[param])
     return <div>
-        <SearchPanel></SearchPanel>
-        <List></List>
+        <SearchPanel param={param} setParam={setParam}></SearchPanel>
+        <List list={list}></List>
     </div>
 }
--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
-export const SearchPanel = (params) => {
-    const [param, setParam] = useState({name:'',personId:''})
+export const SearchPanel = ({param,setParam}) => {
     const [users, setUsers] = useState([])
-    const [list, setList] = useState([])
-
-    useEffect(()=> {
-        fetch('').then(async response=> {
-            if(response.ok) {
-                setList(await response.json())
-            }
-        })
-    },[param])
     return <form>
         <div>
             {/* 浅拷贝 */}
+            {/* setParam(Object.assign({},)) */}
             <input type="text" value={param.name} onChange={evt=>setParam({...param,name:evt.target.value})}/>
             <select name="" id="" value={param.personId} onChange={evt=>setParam({...param,personId:evt.target.value})}>
                 <option value=""></option>
