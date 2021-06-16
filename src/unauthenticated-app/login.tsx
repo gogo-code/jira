68a89743267a5f736c1893b19c00518706162fee
左国梁 <3059349417@qq.com>
2021-06-16 10:26:51 +0800

    优化登录注册页面

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { Form, Input, Button } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 // interface Base {
 //     id:number
@@ -47,9 +48,9 @@ export const LoginScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { Button, Form, Input } from "antd";
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
+import { LongButton } from "unauthenticated-app";
 
 // interface Base {
 //     id:number
@@ -29,7 +30,6 @@ export const RegisterScreen = () => {
         name={"username"}
         rules={[{ required: true, message: "请输入用户名" }]}
       >
-        {" "}
         <Input placeholder={"用户名"} type="text" id={"username"} />
       </Form.Item>
       {/* <div
@@ -44,14 +44,12 @@ export const RegisterScreen = () => {
         name={"password"}
         rules={[{ required: true, message: "请输入密码" }]}
       >
-        {" "}
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        {" "}
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
